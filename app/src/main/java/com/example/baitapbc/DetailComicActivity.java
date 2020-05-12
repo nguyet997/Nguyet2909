7fb99a07973050e838043b6666b074bfc26d880f
Your Name <phamnguyet2909@gmail.com>
2020-05-12 20:19:27 +0700

    first commit

---
+++ app/src/main/java/com/example/baitapbc/DetailComicActivity.java
@@ -0,0 +1,27 @@
+package com.example.baitapbc;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DetailComicActivity extends AppCompatActivity {
+    WebView webView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        webView = new WebView(this);
+        setContentView(webView);
+        webView.getSettings().getJavaScriptEnabled();
+        webView.loadUrl("https://www.truyenngan.com.vn/truyen-ngan/truyen-ngan-yeu.html");
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.getSettings().setUseWideViewPort(true);
+        webView.getSettings().setBuiltInZoomControls(true);
+        webView.getSettings().setDisplayZoomControls(false);
+
+    }
+}
---
+++ app/src/main/java/com/example/baitapbc/WelcomeActivity.java
@@ -0,0 +1,23 @@
+package com.example.baitapbc;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WelcomeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        findViewById(R.id.button_getstart).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            }
+        });
+    }
+}
